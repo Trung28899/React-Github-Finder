46d56ee383b2a88f649923464aa032052308a8bb
Trung Trinh <trung28899@gmail.com>
2020-08-20 18:15:38 -0400

    done the rendering list of output

---
+++ README.txt
@@ -0,0 +1,16 @@
+- All the styling classes of the app is in App.css
+
+- Handling Http requests: using axios
+npm i axios
+handling requests in App.js
+
+- Have to register github application
+follow this guide: 
+https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/
+
+After create it, take: 
+- Client ID
+- Client Secret 
+put to .env.local file
+global variables are used in App.js in componentDidMount()
+in the fetching url
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,31 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export class Navbar extends Component {
-  // default props will props default values in case nothing is passed in as props
-  static defaultProps = {
-    title: " Github Finder",
-    icon: "fab fa-github",
-  };
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon} />
+        {title}
+      </h1>
+    </nav>
+  );
+};
 
-  // determine the data type of props
-  // if something is passed in that is not a string > will get error
-  // in the console
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  };
+// default props will props default values in case nothing is passed in as props
+Navbar.defaultProps = {
+  title: " Github Finder",
+  icon: "fab fa-github",
+};
 
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className={this.props.icon} />
-          {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+// determine the data type of props
+// if something is passed in that is not a string > will get error
+// in the console
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default Navbar;
